64219055c230b704f2751648970cec6fa58f79d5
Jan Baer <jan@janbaer.de>
2018-09-30 16:29:51 +0200

    Some UI improvements for control handling
    - Escape closes the dialog
    - First input element is automatically focussed when open the dialog
    - TextInput is always fuly selected when input is getting the focus
    
    close #1

--- web/src/app/pages/search/components/edit-dialog.js
+++ web/src/app/pages/search/components/edit-dialog.js
@@ -10,10 +10,18 @@ export default class EditDialog extends Component {
 
     this.hide = this.hide.bind(this);
     this.onChangeValue = this.onChangeValue.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+    this.onInputFocus = this.onInputFocus.bind(this);
   }
 
   hide(dialogResult) {
     this.props.onHideDialog(dialogResult, this.state.song);
+  }
+
+  componentDidMount() {
+    if (this.artistInput) {
+      this.artistInput.focus();
+    }
   }
 
   onChangeValue(event) {
@@ -23,25 +31,35 @@ export default class EditDialog extends Component {
     this.setState({ song });
   }
 
+  onKeyPress(e) {
+    if (e.key === 'Escape') {
+      this.hide(false);
+    }
+  }
+
+  onInputFocus(e) {
+    e.target.select();
+  }
+
   render(props, { song }) {
     return (
       <div class="EditDialog-backgroundContainer">
-        <dialog class="EditDialog-dialog" open>
+        <dialog class="EditDialog-dialog" onKeyDown={this.onKeyPress} open>
           <label class="label" for="artist">Artist</label>
-          <input class="input" name="artist" type="text"
-            value={song.artist} onChange={this.onChangeValue} />
+          <input class="input" name="artist" type="text" ref={input => { this.artistInput = input; }}
+            value={song.artist} onChange={this.onChangeValue} onFocus={this.onInputFocus} />
 
           <label class="label" for="title">Title</label>
           <input class="input" name="title" type="text"
-            value={song.title} onChange={this.onChangeValue} />
+            value={song.title} onChange={this.onChangeValue} onFocus={this.onInputFocus} />
 
           <label class="label" for="album">Album</label>
           <input class="input" name="album" type="text"
-            value={song.album} onChange={this.onChangeValue} />
+            value={song.album} onChange={this.onChangeValue} onFocus={this.onInputFocus} />
 
           <label class="label" for="genre">Genre</label>
           <input class="input" name="genre" type="text"
-            value={song.genre} onChange={this.onChangeValue} />
+            value={song.genre} onChange={this.onChangeValue} onFocus={this.onInputFocus} />
 
           <button class="button" onClick={() => this.hide(false)}>Cancel</button>
           <button class="button is-primary" onClick={() => this.hide(true)}>Ok</button>
