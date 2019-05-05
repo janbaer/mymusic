245164a9d9f1495ab952150407c58e87841730c1
Jan Baer <jan@janbaer.de>
2019-05-05 10:34:29 +0200

    Fix problem with entering pasword for each ssh command

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ TARGET_DIR=/volume1/docker/mymusic
 
 MP3DB_SRC=./../mp3db/
 WEB_DIST=./../web/dist/
+PASSWORD=$(pass /home/JABASOFT-DS/jan)
 
 cd ../web
 
@@ -13,22 +14,22 @@ yarn build
 
 cd -
 
-rsync -avuz --progress --delete \
+sshpass -p $PASSWORD rsync -avuz --progress --delete \
   -e "ssh -i ~/.ssh/rsync-key"  \
   --exclude="log/"              \
   --exclude="data/"             \
   --exclude="certificates/"     \
   ./ $USER@$SERVER_NAME:${TARGET_DIR}/
 
-rsync -avuz --progress          \
+sshpass -p $PASSWORD rsync -avuz --progress          \
   -e "ssh -i ~/.ssh/rsync-key"  \
   ${MP3DB_SRC} $USER@$SERVER_NAME:${TARGET_DIR}/mp3db
 
-rsync -avuz --progress          \
+sshpass -p $PASSWORD rsync -avuz --progress          \
   -e "ssh -i ~/.ssh/rsync-key"  \
   ${WEB_DIST} $USER@$SERVER_NAME:${TARGET_DIR}/public
 
-ssh -i ~/.ssh/rsync-key ${USER}@${SERVER_NAME} "cd ${TARGET_DIR}; ./docker-build.sh"
+sshpass -p $PASSWORD ssh -i ~/.ssh/rsync-key ${USER}@${SERVER_NAME} "cd ${TARGET_DIR}; ./docker-build.sh"
 
-ssh -i ~/.ssh/rsync-key ${USER}@${SERVER_NAME} "cd ${TARGET_DIR}; ./docker-run.sh"
+sshpass -p $PASSWORD ssh -i ~/.ssh/rsync-key ${USER}@${SERVER_NAME} "cd ${TARGET_DIR}; ./docker-run.sh"
 
