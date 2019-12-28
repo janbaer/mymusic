58a35ff6cb0bfcc6588166864e018380f5bfe734
Jan Baer <jan@janbaer.de>
2019-12-28 16:36:16 +0100

    Create ssl certificate while deploying and use mounted config files for Caddy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,7 +6,6 @@ TARGET_DIR=/volume1/docker/mymusic
 
 MP3DB_SRC=./../mp3db/
 WEB_DIST=./../web/dist/
-PASSWORD=$(pass /home/JABASOFT-DS/jan)
 
 cd ../web
 
@@ -14,22 +13,22 @@ yarn build
 
 cd -
 
-sshpass -p $PASSWORD rsync -avuz --progress --delete \
+rsync -avuz --progress --delete \
   -e "ssh -i ~/.ssh/rsync-key"  \
   --exclude="log/"              \
   --exclude="data/"             \
   --exclude="certificates/"     \
   ./ $USER@$SERVER_NAME:${TARGET_DIR}/
 
-sshpass -p $PASSWORD rsync -avuz --progress          \
+rsync -avuz --progress          \
   -e "ssh -i ~/.ssh/rsync-key"  \
   ${MP3DB_SRC} $USER@$SERVER_NAME:${TARGET_DIR}/mp3db
 
-sshpass -p $PASSWORD rsync -avuz --progress          \
+rsync -avuz --progress          \
   -e "ssh -i ~/.ssh/rsync-key"  \
   ${WEB_DIST} $USER@$SERVER_NAME:${TARGET_DIR}/public
 
-sshpass -p $PASSWORD ssh -i ~/.ssh/rsync-key ${USER}@${SERVER_NAME} "cd ${TARGET_DIR}; ./docker-build.sh"
+ssh -i ~/.ssh/rsync-key ${USER}@${SERVER_NAME} "cd ${TARGET_DIR}; ./docker-build.sh"
 
-sshpass -p $PASSWORD ssh -i ~/.ssh/rsync-key ${USER}@${SERVER_NAME} "cd ${TARGET_DIR}; ./docker-run.sh"
+ssh -i ~/.ssh/rsync-key ${USER}@${SERVER_NAME} "cd ${TARGET_DIR}; ./docker-run.sh"
 
--- deploy/docker-run.sh
+++ deploy/docker-run.sh
@@ -4,4 +4,6 @@ DOCKER_COMPOSE=/usr/local/bin/docker-compose
 
 ${DOCKER_COMPOSE} down
 
+./create-certificate.sh
+
 PWD=$(pwd) ${DOCKER_COMPOSE} up -d
